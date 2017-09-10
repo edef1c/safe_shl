873014a75c34b3fc92638dd09f28ecdf616f69e2
edef <edef@edef.eu>
2017-09-10 16:13:56 +0200

    add the SafeShl trait

--- lib.rs
+++ lib.rs
@@ -2,14 +2,25 @@
 //! Shift `lhs` left by `rhs` bits, returning `None` instead
 //! if that drops any nonzero bits.
 
+pub trait SafeShl: Sized {
+  /// Shift `self` left by `n` bits, returning `None` instead
+  /// if that drops any nonzero bits.
+  fn safe_shl(self, n: u32) -> Option<Self>;
+}
+
 macro_rules! ty {
   ($ty:ident) => {
-    pub fn $ty(lhs: $ty, rhs: u32) -> Option<$ty> {
-      if rhs <= lhs.leading_zeros() {
-        lhs.checked_shl(rhs)
-      } else {
-        None
+    impl SafeShl for $ty {
+      fn safe_shl(self, n: u32) -> Option<$ty> {
+        if n <= self.leading_zeros() {
+          self.checked_shl(n)
+        } else {
+          None
+        }
       }
+    }
+    pub fn $ty(lhs: $ty, rhs: u32) -> Option<$ty> {
+      lhs.safe_shl(rhs)
     }
   }
 }
